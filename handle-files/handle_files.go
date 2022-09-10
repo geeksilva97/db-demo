44a98be3d612c0c77960916b6c1b7efe68226aba
Edigleysson Silva <edigleyssonsilva@gmail.com>
2022-09-09 23:25:14 -0300

    working with files

---
+++ handle-files/handle_files.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"unsafe"
+)
+
+type myStruct struct {
+  myBool bool // 1 byte
+  myFloat float64 // 8 bytes
+  myInt int32 // 4 bytes
+}
+
+type myOptimizedStruct struct {
+  myFloat float64 // 8 bytes
+  myInt int32 // 4 bytes
+  myBool bool // 1 byte
+}
+
+func main() {
+  a := myStruct{}
+  fmt.Println(unsafe.Sizeof(a)) // 24
+
+  b := myOptimizedStruct{}
+  fmt.Println(unsafe.Sizeof(b)) // 16
+
+  fi, err := os.Open("input.txt")
+
+  if err != nil {
+    panic(err)
+  }
+
+  defer func() {
+    if err := fi.Close(); err != nil {
+      panic(err)
+    }
+  }()
+
+  fo, err := os.Create("output.txt")
+
+  if err != nil {
+    panic(err)
+  }
+
+  defer func() {
+    if err := fo.Close(); err != nil {
+      panic(err)
+    }
+  }()
+
+  buf := make([]byte, 1024)
+
+  for {
+    n, err := fi.Read(buf)
+
+    if err != nil && err != io.EOF {
+      panic(err)
+    }
+
+    if n == 0 {
+      break
+    }
+
+    text_read := string(buf[:n])
+
+    fmt.Printf("Read string: \n\"%s\"\nSize: %v bytes?\nLen: %v\nSizeof: %v\n\n", text_read, int(unsafe.Sizeof(text_read)) + len(text_read), int(unsafe.Sizeof(text_read)), len(text_read))
+
+    if _, err := fo.Write(buf[:n]); err != nil {
+      panic(err)
+    }
+  }
+}
